128e711fa63b3496e1b854f42cae9f6f7754eebf
dylankbuckley <dbuckley14@icloud.com>
2018-04-17 23:20:59 +0100

    Custom port

--- app.js
+++ app.js
@@ -31,4 +31,4 @@ app.get('/', function(req, res){
 
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
