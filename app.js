91a51241bd46c73506c3e2f739118c2022aecd4a
dylankbuckley <dbuckley14@icloud.com>
2018-04-17 23:08:53 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const Avrgirl = require('avrgirl-arduino');
+const serialport = require("serialport")
+
+app.get('/', function(req, res){
+
+  // var avrgirl = new Avrgirl({
+  //   board: 'uno'
+  // });
+  //
+  //
+  //
+  // avrgirl.flash('Blink.cpp.hex', function (error) {
+  //   if (error) {
+  //     console.error(error);
+  //   } else {
+  //     console.info('done.');
+  //   }
+  // });
+
+
+  serialport.list(function (err, ports) {
+  ports.forEach(function(port) {
+    if (port.manufacturer == 'Arduino Srl            ') {
+      console.log("Debut 3D Connected");
+    }
+  });
+});
+
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
